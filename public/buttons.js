dda705453b433802477254f01cbb16eefefa5987
Felix Heyn-Johnsen <fchj@Felixs-MacBook-Pro-2.local>
2014-01-14 20:32:38 +0100

    slider working

---
+++ public/buttons.js
@@ -0,0 +1,16 @@
+$( document ).ready( function() {
+    
+    $('#grab').change(function() {
+        console.log($(this).val());
+        $.ajax({
+            url: '/dof/3',
+            type: 'PUT',
+            data: 'position='+$(this).val(),
+            success: function(res){
+                console.log(res);
+            }
+        });
+    });
+                      
+});
+                 
