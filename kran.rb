2839e76509530aafdb8d496f3e9bd983bc1ce171
Felix Heyn-Johnsen <fchj@Felixs-MacBook-Pro-2.local>
2014-01-16 22:16:28 +0100

    all

--- kran.rb
+++ kran.rb
@@ -3,3 +3,20 @@ require 'sinatra'
 get '/hi' do
   "Hello from kran"
 end
+
+#all of it
+get '/dof' do
+
+end
+
+#specific servo channel state, running, stopped, sensor state, etc.
+get '/dof/:id' do
+
+end
+
+#set specific servo/engine to something
+put '/dof/:id' do
+  #request.body.rewind
+  #request.params.inspect
+  "hi you moved %s to %s" % [params[:id], params[:position]]
+end
--- public/buttons.js
+++ public/buttons.js
@@ -11,6 +11,8 @@ $( document ).ready( function() {
             }
         });
     });
+    
+    $('#').change(function(){});
                       
 });
                  
